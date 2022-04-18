1f00ee20a5dfa59562367b598a1625014e261efd
ola <raymond.oyinlola@gmail.com>
2022-04-18 09:38:11 +0800

    confirm the shared kernel works

--- ticketing/auth/src/app.controller.ts
+++ ticketing/auth/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+
+import { AppService } from '@tickets-microservice/ticket-shared-kernel';
 
 @Controller()
 export class AppController {
